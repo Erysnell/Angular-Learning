d52b0f63f5e9697ddb7e974f54bb65b01645b61d
Erysnell <erysnell09@gmail.com>
2024-04-18 15:15:40 +0200

    aprendiendo a manejar interpolacion

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-heroes-hero',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
 
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    const newName = prompt('Introduce el nuevo nombre del héroe:');
+    if (newName) {
+      this.name = String(newName);
+  }
+}
+
+  changeAge() {
+    const newAge = prompt('Introduce la nueva edad del héroe:');
+    if (newAge) {
+      this.age = Number(newAge);
+    }
+  }
 }
