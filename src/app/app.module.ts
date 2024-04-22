b1a775595cee637fc7878f7407e249754d0935a1
Erysnell <erysnell09@gmail.com>
2024-04-22 15:14:41 +0200

    encapsulando los componentes en sus modulos, y exportandolos al modulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
-import { CounterComponent } from './counter/counter.component';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/components/counter.module';
+import { HeroesMoudule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
+    CounterModule,
+    HeroesMoudule,
     AppRoutingModule
   ],
   providers: [],
